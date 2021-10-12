63d33b577b6fd02616cb4c62f02277bd5bb8771c
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2021-10-12 13:26:15 +0900

    move files

--- database/DUMP.sql
+++ database/DUMP.sql
@@ -202,23 +202,21 @@ f0759461-f690-4530-b35d-8246933539d5    6e9bf5c3-3da3-4e00-b912-68b9fe6d24a1    
 -- Data for Name: product; Type: TABLE DATA; Schema: nile; Owner: nile
 --
 
-COPY nile.product (id) FROM stdin;
-99a8e0c2-0ed3-426d-911d-eca5e67918f8
-6e9bf5c3-3da3-4e00-b912-68b9fe6d24a1
-699140cc-bd30-412f-a574-dc1272a48ba7
-\.
+INSERT INTO nile.product VALUES
+('99a8e0c2-0ed3-426d-911d-eca5e67918f8'),
+('6e9bf5c3-3da3-4e00-b912-68b9fe6d24a1'),
+('699140cc-bd30-412f-a574-dc1272a48ba7');
 
 
 --
 -- Data for Name: product_history; Type: TABLE DATA; Schema: nile; Owner: nile
 --
 
-COPY nile.product_history (product_id, version_number, name, price) FROM stdin;
-99a8e0c2-0ed3-426d-911d-eca5e67918f8    0       iPhone 13 128GB 100000
-6e9bf5c3-3da3-4e00-b912-68b9fe6d24a1    0       iPhone 13 256GB 120000
-699140cc-bd30-412f-a574-dc1272a48ba7    0       iPhone 13 1TB   170000
-99a8e0c2-0ed3-426d-911d-eca5e67918f8    1       iPhone 13 128GB 98000
-\.
+INSERT INTO nile.product_history (product_id, version_number, name, price) VALUES
+('99a8e0c2-0ed3-426d-911d-eca5e67918f8', 0, 'iPhone 13 128GB', 100000),
+('6e9bf5c3-3da3-4e00-b912-68b9fe6d24a1', 0, 'iPhone 13 256GB', 120000),
+('699140cc-bd30-412f-a574-dc1272a48ba7', 0, 'iPhone 13 1TB', 170000),
+('99a8e0c2-0ed3-426d-911d-eca5e67918f8', 1, 'iPhone 13 128GB', 98000);
 
 
 --
