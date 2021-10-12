2f472d9c3c3d808c23ec45ec75b113185fef9864
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2021-10-12 18:46:04 +0900

    Fix sql

---
+++ database/SETUP.sql
@@ -0,0 +1,11 @@
+INSERT INTO nile.product VALUES
+('99a8e0c2-0ed3-426d-911d-eca5e67918f8'),
+('6e9bf5c3-3da3-4e00-b912-68b9fe6d24a1'),
+('699140cc-bd30-412f-a574-dc1272a48ba7');
+
+
+INSERT INTO nile.product_history (product_id, version_number, name, price) VALUES
+('99a8e0c2-0ed3-426d-911d-eca5e67918f8', 0, 'iPhone 13 128GB', 100000),
+('6e9bf5c3-3da3-4e00-b912-68b9fe6d24a1', 0, 'iPhone 13 256GB', 120000),
+('699140cc-bd30-412f-a574-dc1272a48ba7', 0, 'iPhone 13 1TB', 170000),
+('99a8e0c2-0ed3-426d-911d-eca5e67918f8', 1, 'iPhone 13 128GB', 98000);
